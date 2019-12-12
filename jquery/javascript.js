f46810a684d7c066c62297db15650c52149c309d
titoanharits <38245796+titoanharits@users.noreply.github.com>
2019-12-13 00:22:42 +0700

    90%

--- jquery/javascript.js
+++ jquery/javascript.js
@@ -3,6 +3,7 @@ var modal = document.getElementById("myModal");
 var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
+
 btn.onclick = function() {
   modal.style.display = "block";
 }
@@ -17,6 +18,7 @@ window.onclick = function(event) {
   }
 }
 
+
 //image upload view admin
 var loadFile = function(event) {
   var image = document.getElementById('output');
@@ -29,13 +31,13 @@ $(document).ready(function(){
   $("#load").click(function(){
     $.ajax({
       type: 'GET',
-      url: 'https://raw.githubusercontent.com/HafizArdi/HafizArdi.github.io/master/views/data.json',
+      url: 'https://raw.githubusercontent.com/titoanharits/github.io/master/databarang.json',
       data: { get_param: 'value' },
       dataType: 'json',
       success: function (data) {
         $.each(data, function(index, elem) {
           console.log('product '+elem['product']+' - description '+elem['description']+' - price '+elem['price']+' - stock '+elem['stock']+' - image '+elem['image'])
-          $('#destination').prepend('<section><a href="singleProduct.html"><img src="img/'+elem['image']+'" /></a><h2>'+elem['product']+'</h2><p>'+elem['description']+'</p><aside><ul><li>Price: '+elem['price']+'</li><li>'+elem['stock']+'</li></ul><button>Add to Cart</button></aside></section>');
+          $('#destination').prepend('<section><a href="singleProduct.html"><img src="img/'+elem['image']+'" /></a><h2>'+elem['product']+'</h2><p>'+elem['description']+'</p><aside><ul><li>Price: '+elem['price']+'</li><li>'+elem['stock']+'</li></ul><a href="singleProductAdmin.html"><button>Details</button></a></aside></section>');
         });
       }
     });
